9d7fe839890eedfbf2b883eadeb697414c9e9023
Kishokanth-netizen <80698644+Kishokanth-netizen@users.noreply.github.com>
2021-06-18 17:39:27 +0200

    local sw working

--- iogt/templates/sw.js
+++ iogt/templates/sw.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
+importScripts('../static/third_party/workbox/workbox-v6.1.5/workbox-sw.js');
 
 // The below implementation will basically convert "{{ test_url }}" which is test/ into http://localhost:8000/test
 // The relative paths such as test/ of the pages inside the app will be provided by the backend devs, 
