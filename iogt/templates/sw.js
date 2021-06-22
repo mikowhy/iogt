65de17b17a9563514bce6f0018f4c5c6415818b3
Kishokanth-netizen <80698644+Kishokanth-netizen@users.noreply.github.com>
2021-06-22 15:49:30 +0200

    event listener working

--- iogt/templates/sw.js
+++ iogt/templates/sw.js
@@ -1,4 +1,5 @@
-importScripts('../static/third_party/workbox/workbox-v6.1.5/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
+const {precacheAndRoute}  = workbox.precaching;
 
 // The below implementation will basically convert "{{ test_url }}" which is test/ into http://localhost:8000/test
 // The relative paths such as test/ of the pages inside the app will be provided by the backend devs, 
@@ -8,8 +9,16 @@ const appShell = [
   "{{ test_url }}",
 ].map((partialUrl) => `${location.protocol}//${location.host}${partialUrl}`);
 
-workbox.precaching.precacheAndRoute(appShell.map(url => ({
-  url,
-  revision: null,
-})));
+
+addEventListener('message', async (event) => {
+  if (event.data.type === 'CACHE') {
+   
+    precacheAndRoute(appShell.map(url => ({
+      url,
+      revision: null,
+    })));
+
+   console.log(appShell)
+  }
+});
 
