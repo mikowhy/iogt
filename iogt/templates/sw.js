9b11b05634c015ec30a003fb1d750345027d5830
Kishokanth-netizen <80698644+Kishokanth-netizen@users.noreply.github.com>
2021-06-23 09:55:07 +0200

    removed button error

--- iogt/templates/sw.js
+++ iogt/templates/sw.js
@@ -1,24 +1,36 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
-const {precacheAndRoute}  = workbox.precaching;
+const { precacheAndRoute }  = workbox.precaching;
 
-// The below implementation will basically convert "{{ test_url }}" which is test/ into http://localhost:8000/test
-// The relative paths such as test/ of the pages inside the app will be provided by the backend devs, 
-// so this will be done programmatically
 
 const appShell = [
   "{{ test_url }}",
 ].map((partialUrl) => `${location.protocol}//${location.host}${partialUrl}`);
 
+// self.addEventListener('install', async (event) => {
+//   console.log('Installed!');
+//   self.skipWaiting();
+// });
 
-addEventListener('message', async (event) => {
-  if (event.data.type === 'CACHE') {
-   
-    precacheAndRoute(appShell.map(url => ({
-      url,
-      revision: null,
-    })));
+// self.addEventListener('activate', event => {
+//   console.log('V1 now ready to handle fetches!');
+  
+// });
 
-   console.log(appShell)
-  }
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    console.log('installed')
+  );
 });
 
+self.addEventListener('activate', event => {
+  console.log('V1 now ready to handle fetches!');
+});
+
+
+self.addEventListener('message', async (event) => {
+  precacheAndRoute(appShell.map(url => ({
+    url,
+    revision: null,
+  })));
+});
